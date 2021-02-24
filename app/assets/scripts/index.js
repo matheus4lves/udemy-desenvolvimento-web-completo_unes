4a03cdfdf5e3f9635c9d9a52bd0eb1dcf75d9409
Matheus Alves <matheusalves.bmd@gmail.com>
2021-02-24 17:19:14 -0300

    Implement the "Contato" page

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -1,5 +1,7 @@
 import '../styles/styles.css';
 import '../../index.html';
+import '../../common/quem-somos.html';
+import '../../common/contato.html';
 
 if(module.hot) {
     module.hot.accept('./index.js');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const webpack = require('webpack'); // to access built in plugins
+// to access built in plugins
+const webpack = require('webpack');
+
 const path = require('path');
 
 module.exports = {
@@ -7,29 +9,40 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'app'),
     },
+
     // Tell me exactly where an error has occurred
     devtool: 'inline-source-map',
+
     devServer: {
         contentBase: './app',
-        host: '0.0.0.0', // makes the server externally accessible
+        // enable HMR
+        hot: true,
+
+        // makes the server externally accessible
+        host: '0.0.0.0',
+
         // in you phone's browser type: <yourComputerLocalIp>:9000
         port: 9000,
-        hot: true, // enable HMR
+
+        // display your local ip in the address bar
+        useLocalIp: true,
+
+        // Tells devServer to write generated assets to the disk.
+        // for example, the bundle.js asset
+        // writeToDisk: true,
+
         //index: './app/index.html', what is it for?
         open: {
             app: ['brave-browser', '--incognito'],
         },
-        // display in the browser your computer's local ip
-        useLocalIp: true,
-        // Tells devServer to write generated assets to the disk.
-        // for example, the bundle.js asset
-        // writeToDisk: true,
     },
     module: {
         rules: [
             {
                 test: /\.html$/,
-                type: 'asset/source', // replaces <use: raw-loader>
+
+                // replaces <use: raw-loader>
+                type: 'asset/source',
             },
             {
                 test: /\.css$/,
@@ -37,7 +50,6 @@ module.exports = {
             },
         ],
     },
-    // Does it make the browser apply CSS on the fly?
     plugins: [
         new webpack.HotModuleReplacementPlugin({}),
     ],
